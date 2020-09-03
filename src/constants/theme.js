c4421030f573fb58deab4e66cdf8f43554ad3d39
angelgarzz <angelgarzon42@gmail.com>
2020-09-03 15:35:34 -0400

    Theme constants

---
+++ src/constants/theme.js
@@ -0,0 +1,29 @@
+export const COLORS = {
+    background: {
+        light: '#fff', // White
+        dark: '#000', // Black
+    },
+    background2: {
+        light: '#dfebf6', // White
+        dark: '#1f1f30', // Black
+    },
+    text: {
+        light: '#222', // Near black
+        dark: '#fff', // White
+    },
+    title: {
+        light: '#333',
+        dark: '#f2f5f7',
+    },
+    primary: {
+        light: '#0077ff', // Blue
+        dark: '#0077ff', // Blue
+    },
+    soft: {
+        light: '#eee', // Blue
+        dark: '#383838', // Blue
+    }
+};
+  
+export const COLOR_MODE_KEY = 'color-mode';
+export const INITIAL_COLOR_MODE_CSS_PROP = '--initial-color-mode';
